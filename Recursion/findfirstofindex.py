df5c41b8999df5a6c73099f7418dbc01107a60bc
mahima-c <13as1827000713@gmail.com>
2020-08-30 00:59:54 +0530

    Arrange_folder

---
+++ Recursion/findfirstofindex.py
@@ -0,0 +1,12 @@
+def firstIndex(arr, x):
+    # Please add your code here
+
+    
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+x=int(input())
+print(firstIndex(arr, x))
